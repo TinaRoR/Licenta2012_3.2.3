e400c57f532e164e1f15704894cd21645e176e0f
Florentina <gemalescuflorentina@live.com>
2012-05-30 21:38:17 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Licenta2012::Application.config.secret_token = '2e3e057249a21d1e8907694e2002ead01f607ec5adf61b86145d0209bcd5297acb9dcc601d6a41d5376427e1d92ad3ace16340ffc996469093f42eede70ca4e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Licenta2012::Application.config.session_store :cookie_store, key: '_Licenta2012_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Licenta2012::Application.config.session_store :active_record_store
