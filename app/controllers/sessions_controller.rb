37e1c6767261f556ac45acaccf7090e1e539c9a7
Florentina <gemalescuflorentina@live.com>
2012-06-24 22:43:01 +0300

    Finished user edit, update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 
 class SessionsController < ApplicationController
+  before_filter :already_signed_in, only: [:new, :create]
   
   def new
     end
@@ -9,7 +10,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       #Create an error message and re-render the signin form.
       flash.now[:error] = 'Invalid email/password combination'
@@ -23,5 +24,11 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
+  
+  private
+  
+    def already_signed_in
+      redirect_to root_path unless !signed_in?
+    end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 
 class UsersController < ApplicationController
+  before_filter :signed_in_user,    only: [:edit, :update, :index, :destroy]
+  before_filter :correct_user,      only: [:edit, :update]
+  before_filter :admin_user,        only: :destroy
+  before_filter :already_signed_in, only: [:new, :create]
   
   def show
     @user = User.find(params[:id])
@@ -19,4 +23,53 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+   #@user = User.find(params[:id]) not needed anymore because of the filter correct_user
+  end
+  
+  def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy unless User.find(params[:id]).admin?
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
+  
+  private
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in." 
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def already_signed_in
+      redirect_to root_path unless !signed_in?
+    end
 end
